22dc22ac103463a672140005c7454d28cf62113b
alexsalniy <alexsalniy@gmail.com>
2023-11-16 17:42:43 +0300

    Add social login with Google and Github accounts

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,17 +1,23 @@
-'use client';
+"use client";
 
-import Image from 'next/image'
+import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+  src
+}) => {
   return (
     <Image
-      className='rounded-full'
-      height='30'
-      width='30'
-      alt='Avatar'
-      src='/images/placeholder.jpg'
+      className="rounded-full"
+      height="30"
+      width="30"
+      alt="Avatar"
+      src={src || "/images/placeholder.jpg"}
     />
-  )
-}
+  );
+};
 
 export default Avatar;
